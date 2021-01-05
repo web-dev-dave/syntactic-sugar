86c983a9eac16a166ac33e7d0b2cef2563e299b9
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2021-01-05 20:34:17 +1300

    setting functions inside the class constructor

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 // Using Classes
 class Person {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, dob) {
     this.firstName = firstName,
     this.lastName = lastName
+    this.dob = new Date(dob)
+  }
+
+  greeting() {
+    return `Good afternoon ${this.firstName} ${this.lastName}`
+  }
+
+  calculateAge(date) {
+    const year = new Date().getFullYear()
+    console.log(year)
+    return year - date
   }
 }
 
-const jeff = new Person('Jeff', 'Scott');
+const jeff = new Person('Jeff', 'Scott', '11-8-1984');
 
 console.log(jeff)
+console.log(jeff.greeting())
+console.log(jeff.calculateAge(1984))
