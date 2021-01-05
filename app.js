b81c18165a47e256a20427cc8513eed3f554d72f
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2021-01-05 21:07:23 +1300

    using functions withing classes

--- app.js
+++ app.js
@@ -3,22 +3,39 @@ class Person {
   constructor(firstName, lastName, dob) {
     this.firstName = firstName,
     this.lastName = lastName
-    this.dob = new Date(dob)
+    this.birthday = new Date(dob)
   }
 
   greeting() {
     return `Good afternoon ${this.firstName} ${this.lastName}`
   }
 
-  calculateAge(date) {
-    const year = new Date().getFullYear()
-    console.log(year)
-    return year - date
+  // Works but is greatly flawed needs argument
+  // calculateAge(date) {
+  //   const year = new Date().getFullYear()
+  //   console.log(year)
+  //   return year - date
+  // }
+
+  // Far more effective
+  calculateAge() {
+    const diff = Date.now() - this.birthday.getTime()
+
+    const ageDate = new Date(diff)
+
+    return Math.abs(ageDate.getUTCFullYear() - 1970)
+  }
+
+  getsMarried(newLastName) {
+    this.lastName = newLastName
   }
 }
 
-const jeff = new Person('Jeff', 'Scott', '11-8-1984');
+const jess = new Person('Jess', 'Scott', '11-8-1984');
+console.log(jess)
 
-console.log(jeff)
-console.log(jeff.greeting())
-console.log(jeff.calculateAge(1984))
+jess.getsMarried('Bright')
+
+console.log(jess)
+console.log(jess.greeting())
+console.log(jess.calculateAge())
