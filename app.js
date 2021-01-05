0bf2372d77d70f2094f64154b6448a2d24717b0c
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2021-01-05 20:25:16 +1300

    using classes

---
+++ app.js
@@ -0,0 +1,11 @@
+// Using Classes
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName,
+    this.lastName = lastName
+  }
+}
+
+const jeff = new Person('Jeff', 'Scott');
+
+console.log(jeff)
